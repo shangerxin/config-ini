38354b11011dbbbe31b98b3391796d049fc35618
mftrips <36311744+mftrips@users.noreply.github.com>
2018-02-09 14:27:23 -0600

    allow lines that contain hash or semicolon

--- config-ini.js
+++ config-ini.js
@@ -36,7 +36,7 @@
     var DEFAULT_SECTION    = "__DEFAULT_SECTION__";
     var _sectionRegex      = /\s*\[\s*(\S+)\s*\]\s*/;
     var _optionRegex       = /\s*(\S+)\s*[=:]\s*(.*)\s*/;
-    var _commentRegex      = /\s*[#;].*/;
+    var _commentRegex      = /^\s*[#;].*/;
     var _emptyRegex        = /^\s*$/;
     var SECTION_NAME_INDEX = 1;
     var OPTION_NAME_INDEX  = 1;
