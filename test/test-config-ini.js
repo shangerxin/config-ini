557344af375203408e7674ce8841cd5fa586c1fd
erxin <shangerxin@hotmail.com>
2020-07-08 07:38:30 +0800

    Update version number and readme to change the class type definition to under the module

--- test/test-config-ini.js
+++ test/test-config-ini.js
@@ -4,7 +4,7 @@
  * The license is under GPL-3.0
  * Git repo:https://github.com/shangerxin/config-ini
  * Author homepage: http://www.shangerxin.com
- * Version, 1.3.2
+ * Version, 1.3.3
  */
 var ConfigIniParser = require("../config-ini").ConfigIniParser;
 var expect = require("chai").expect;
